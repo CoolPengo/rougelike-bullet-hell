5f348be94835b9797a31e30402c974a66825ccbc
CoolPengo <120464264+CoolPengo@users.noreply.github.com>
2026-01-22 21:32:12 +0100

    Beninging of entity system

---
+++ src/entitysystem/entity.py
@@ -0,0 +1,27 @@
+
+
+class Entity():
+    def __init__(self, name, components=None):
+        self.name = name or "UnnamedEntity"
+        
+    def get_components(self):
+        return self.components
+
+    def get_component(self, component):
+        for c in self.components:
+            if isinstance(c, component):
+                return c
+
+    def has_component(self, component):
+        for c in self.components:
+            if isinstance(c, component):
+                return True
+        return False
+    
+    def add_component(self, component):
+        component.owner = self
+        self.components.append(component)
+
+    def update(self, dt):
+        for c in self.components:
+            c.update(dt)
---
+++ src/entitysystem/entityComponent.py
@@ -0,0 +1,6 @@
+class EntityComponent:
+    def __init__(self, owner=None):
+        self.owner = owner
+
+    def update(self, dt):
+        pass

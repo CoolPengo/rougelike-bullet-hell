d25084ba74bc055e868b1688372de0920f53ecc8
Mhiw <103310488+Mhiw@users.noreply.github.com>
2026-01-24 10:57:44 +0100

    Added collider as a component

--- src/entityComponents/collider_component.py
+++ src/entityComponents/collider_component.py
@@ -1,10 +1,13 @@
-class Collider():
+from entitySystem.entity_components import EntityComponents
+
+class ColliderComponent(EntityComponents):
     def __init__(self, x, y, w, h):
+        super().__init__()
         self.x = x
         self.y = y
         self.w = w
         self.h = h
         self.is_colliding = False
 
-    def EvaluateCollision(self, collider):
+    def evaluate_collision(self, collider):
         self.is_colliding = self.x + self.w > collider.x and self.x < collider.x + collider.w and self.y + self.h > collider.y and self.y < collider.y + collider.h

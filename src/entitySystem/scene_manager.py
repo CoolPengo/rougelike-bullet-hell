420a2dec1a7039991ea82bd0c560c774f7e3d0a8
Mhiw <103310488+Mhiw@users.noreply.github.com>
2026-01-26 09:26:00 +0100

    Fixed comment formatting

--- src/entitySystem/scene_manager.py
+++ src/entitySystem/scene_manager.py
@@ -1,4 +1,4 @@
-'''''class SceneManager:
+'''class SceneManager:
     def __init__(self):
         self.entities = []
 
@@ -18,5 +18,5 @@
     def draw(self, surface):
         for e in self.entities:
             e.draw(surface)
-'''''
+'''
 # IDK if this'll be useful so commenting it out rn and might remove l8r
